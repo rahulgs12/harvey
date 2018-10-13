747cebc8176f1b049e611cf9ed033a0589511c49
shamystic <ssh50@duke.edu>
2018-10-13 18:16:04 -0400

    added flask app

---
+++ README.md
@@ -0,0 +1,4 @@
+# Harvey 
+
+
+Harvey is a end-to-end system that allows remote, disconnected areas to share files with one another. Remote Raspbery Pi systems can connect to a web server and 
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome to Harvey!'
+
+@app.route('/upload')
+def upload():
+    return 'Upload files'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
