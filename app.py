d21caee678a5f462094897319a1f1ef6360ed51b
shamystic <ssh50@duke.edu>
2018-10-13 18:25:27 -0400

    added allowed files

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app = Flask(__name__)
 
