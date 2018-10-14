3a0f8354966f935e8f10c6c531776fcb8da4ddb4
shamystic <ssh50@duke.edu>
2018-10-13 23:12:17 -0400

    added download feature

--- app.py
+++ app.py
@@ -1,52 +1,69 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, abort, send_file
 from werkzeug.utils import secure_filename
 from jinja2 import Template
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DB_URL'] = '/'
-# db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods = ['POST'])
-def upload():
-    file = request.files['inputFile']
-    return file.filename
-#     # newFile = FileContents(name = file.filename, data = file.read())
-#     # db.session.add(newFile)
-#     # db.session.commit()
-#     # return ("Saved " + file.filename + ' to the database!')
+# @app.route('/upload', methods = ['POST'])
+# def upload():
+#     file = request.files['inputFile']
+#     return file.filename
+# #     # newFile = FileContents(name = file.filename, data = file.read())
+# #     # db.session.add(newFile)
+# #     # db.session.commit()
+# #     # return ("Saved " + file.filename + ' to the database!')
 
 @app.route('/form')
 def form():
     return render_template('forms.html')
 
-# class FileContents(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     name = db.Column(db.String(300))
-#     data = db.Column(db.LargeBinary)
-
 @app.route('/new', methods=['POST'])
 def new():
-    print(request.form)
-    # print(request.args['lastname'])
- #   file = request.files['inputFile']
-    #return 'new user added', file.filename
+    print(request.form)    
+    # return 'new user added', file.filename
     file = request.files['inputFile']
-    os.mkdir(request.form['patientID'])
-    file.save(request.form['patientID']+'/'+secure_filename(file.filename))
-    # newFile = FileContents(name = file.filename, data = file.read())  # file.read() gets the file itself
-    # db.session.add(newFile)
-    # db.session.commit()
-    return ('Saved ' + file.filename)
+    dest_path = 'uploads/' + request.form['patientID'] + '/'
+    if not os.path.isdir(dest_path): 
+        os.makedirs(dest_path)
+    file.save(dest_path + secure_filename(file.filename))
+    return ('Saved ' + file.filename + ' for ' + request.form['firstname'])
 
 # @app.route('/user/<username>')
 # def show_user(username):
 #     user = User.query.filter_by(username=username).first_or_404()
 #     return render_template('show_user.html', user=user)
+
+# @app.route('/', defaults={'req_path': ''})
+@app.route('/<user>')
+def dir_listing(user):
+    BASE_DIR = 'uploads/'
+    # # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, user)
+    print (abs_path)
+    # # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+    # # Show directory contents
+    files = os.listdir(abs_path)
+    files = [abs_path + '/'+ f  for f in files]
+    return render_template('files.html', files = files)
+
+@app.route('/uploads/<user>/<filename>') 
+def get_file(user, filename): 
+    BASE_DIR = 'uploads/'
+    abs_path = os.path.join(BASE_DIR, user, filename)
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)        
+    else: 
+        return abort(404)
+    return abs_path
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
