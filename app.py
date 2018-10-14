ced710d5bd1860ce5d991207d3b88e5f0c1083a9
shamystic <ssh50@duke.edu>
2018-10-13 21:44:01 -0400

    added file upload

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ def index():
 def upload():
     file = request.files['inputFile']
     return file.filename
-    # newFile = FileContents(name = file.filename, data = file.read())
-    # db.session.add(newFile)
-    # db.session.commit()
-    # return ("Saved " + file.filename + ' to the database!')
-    
+#     # newFile = FileContents(name = file.filename, data = file.read())
+#     # db.session.add(newFile)
+#     # db.session.commit()
+#     # return ("Saved " + file.filename + ' to the database!')
+
 @app.route('/form')
 def form():
     return render_template('forms.html')
@@ -29,13 +29,22 @@ def form():
 #     name = db.Column(db.String(300))
 #     data = db.Column(db.LargeBinary)
 
+@app.route('/new', methods=['POST'])
+def new():
+    # print(request.args['lastname'])
+ #   file = request.files['inputFile']
+    #return 'new user added', file.filename 
+    file = request.files['inputFile']
+    file.save(secure_filename(file.filename))
+    # newFile = FileContents(name = file.filename, data = file.read())  # file.read() gets the file itself 
+    # db.session.add(newFile)
+    # db.session.commit()
+    return ('Saved ' + file.filename)
 
-
-# @app.route('/new', methods=['GET'])
-# def new_user():
-#     print(request.args['lastname'])
-#     file = request.files['inputFile']
-#     return 'new user added', file.filename 
+# @app.route('/user/<username>')
+# def show_user(username):
+#     user = User.query.filter_by(username=username).first_or_404()
+#     return render_template('show_user.html', user=user)    
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
